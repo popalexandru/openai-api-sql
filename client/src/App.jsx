af0da800658ace765824ae9e1ae5726d7cb6ee74
popalexandru <popalexandru1197@gmail.com>
2025-09-06 23:24:53 +0300

    Added submit page which currently logs the query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
+import styles from "./index.module.css";
+import sqlLogo from "./assets/sqlLogo.png";
+
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [queryDescription, setQueryDescription] = useState("");
 
-  return <h1>Helelo World</h1>;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted !", queryDescription);
+
+    setQueryDescription("Describe your query");
+  };
+
+  return (
+    <main className={styles.main}>
+      <img src={sqlLogo} alt="" className={styles.icon} />
+      <h3>Generate SQL with AI</h3>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          onChange={(e) => setQueryDescription(e.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+      </form>
+    </main>
+  );
 }
 
 export default App;
